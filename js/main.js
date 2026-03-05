39b6a1494a6af9580a4a26212fe2c0803e45fb53
drewrobel190-hash <drewrobel190@gmail.com>
2026-03-05 14:34:34 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1868,15 +1868,11 @@ function limitBossHistory(){
     });
 }
 
-
-setTimeout(() => {
-   sendDiscordAlert("⚔️ TEST BOSS SPAWN TIMER WORKING");
-}, 10000);
-
 // ===== CREATE WORLD BOSSES =====
 
 createWorldBossCard("Ratan", 60, "Pictures/World boss/Ratan.png", "Tomb of Time");
 createWorldBossCard("Parto", 85, "Pictures/World boss/Parto.png", "Magic Puppet's Yearning");
 createWorldBossCard("Nedra", 105, "Pictures/World boss/Nedra.png", "Bloodsoaked Plateau");
+
 
 
